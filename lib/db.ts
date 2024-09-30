28f45a7a640a7a559b964b9d55b296a534bbc1ab
Tasleemofx <oladepotesleem5@gmail.com>
2024-09-30 17:27:50 +0100

    prisma instance

--- lib/db.ts
+++ lib/db.ts
@@ -9,8 +9,8 @@ declare const globalThis:{
     prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
+    const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+    
 
 export default prisma
 
