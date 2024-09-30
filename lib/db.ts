f5147aee32dfb943a1ce6a3870612b7ec55da7c8
Tasleemofx <oladepotesleem5@gmail.com>
2024-09-30 17:32:17 +0100

    disconnecting prisma

--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,8 @@ declare const globalThis:{
 
     const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
     
-
+    prisma.$disconnect()
+    
 export default prisma
 
 if(process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
